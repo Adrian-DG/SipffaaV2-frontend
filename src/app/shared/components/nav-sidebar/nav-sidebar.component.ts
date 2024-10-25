4b9966f6987967def8616b82246bb2775d3be8ff
Adrian-DG <adrian96guilamo@gmail.com>
2024-10-24 22:47:22 -0400

    sidevar menu in progress

--- src/app/shared/components/nav-sidebar/nav-sidebar.component.ts
+++ src/app/shared/components/nav-sidebar/nav-sidebar.component.ts
@@ -1,16 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 import { IUrlOption } from '../../models/iurl-option.model';
+import { modules } from '../../../app.modules';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
 	selector: 'app-nav-sidebar',
 	standalone: true,
-	imports: [SidebarModule, MenuModule],
+	imports: [SidebarModule, MenuModule, DividerModule],
 	templateUrl: './nav-sidebar.component.html',
 	styleUrl: './nav-sidebar.component.scss',
 })
 export class NavSidebarComponent {
 	@Input() isVisible!: boolean;
-	@Input() modules!: IUrlOption[];
+	modules: IUrlOption[] = modules;
 }
